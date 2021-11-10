d4e3d2fbf2bcfb27fa619b4048f977ce18c2ee89
Lukas Graf <lukas.graf@math.uni-augsburg.de>
2021-11-10 01:21:25 +0100

    Aufgabe 1 angefangen

---
+++ src/exercise1.lean
@@ -0,0 +1,52 @@
+import order.galois_connection
+
+universe u
+variables X Y : Type u
+
+def VR {X Y} (R: X → Y → Prop) (S : set X) : set Y := {y : Y | ∀ (s ∈ S), R s y}
+def IR {X Y} (R: X → Y → Prop) (T : set Y) : set X := {x : X | ∀ (t ∈ T), R x t}
+
+def subsetrel {X} : preorder (set X) := {
+  le := λ A B, A ⊆ B,
+  le_refl := set.subset.refl,
+  le_trans := begin
+    intros A B C,
+      dsimp,
+      exact set.subset.trans,
+  end,
+}
+def supsetrel {Y} : preorder (set Y) := {
+  le := λ A B, A ⊇ B,
+  le_refl := set.subset.refl,
+  le_trans := begin
+    intros A B C,
+      dsimp,
+      intros hAB hBC,
+      apply set.subset.trans,
+      exact hBC,
+      exact hAB,
+  end,
+}
+
+lemma ex1a (R : X → Y → Prop): @galois_connection (set X) (set Y) subsetrel supsetrel (VR R) (IR R) := begin
+  intros S T,
+  split,
+  {
+    show VR R S ⊇ T → S ⊆ IR R T,
+    intro h,
+    intros s hs,
+    intros t ht,
+    specialize h ht,
+    specialize h s hs,
+    exact h,
+  },
+  {
+    show S ⊆ IR R T → VR R S ⊇ T,
+    intro h,
+    intros t ht,
+    intros s hs,
+    specialize h hs,
+    specialize h t ht,
+    exact h,
+  },  
+end
